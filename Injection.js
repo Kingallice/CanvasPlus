90fd8856ee7268008f27e9ddbda364d7f8b60f27
Noah <noahmosel73@gmail.com>
2022-09-17 17:02:32 -0500

    Added Grade to Class Header
    Shows grades for a class when you open up the class page

--- Injection.js
+++ Injection.js
@@ -1,51 +1,98 @@
+mainCanvasURL = 'https://'+window.location.href.split('/')[2];
+
+function getGradeText(jsonResponse){
+	if(jsonResponse["enrollments"][0]["computed_current_score"] != null){
+		return[jsonResponse["name"],jsonResponse["enrollments"][0]["computed_current_score"] + "%"];
+	}
+	else {
+		return [jsonResponse["name"],"N/A"];
+	}
+}
+
 function CanvasGradeOverlays() {
-	function httpGet()
-	{
-		var xmlHttp = new XMLHttpRequest();
-		xmlHttp.open( "GET", window.location.href+'/api/v1/users/self/courses?include[]=total_scores&enrollment_state=active&per_page=20', false ); // false for synchronous request
-		xmlHttp.send( null );
-		getParts(xmlHttp.response);
-	}
-	function getParts(response){
-		response = JSON.parse(response);
-		response.forEach(function t(x){
-			if(x["enrollments"][0]["computed_current_score"] != null){
-				setGrade(x["name"],x["enrollments"][0]["computed_current_score"] + "%");
-			}
-			else {
-				setGrade(x["name"],"N/A");
-			}
-		});
-	}
-	function setGrade(name, grade){
-		elementArr = document.getElementsByClassName("ic-DashboardCard");
-		for(let i = 0; i < elementArr.length; i++){
-			x = elementArr[i];
-			if(name == x.getAttribute("aria-label")){
-				a = x.children[0];
-				b = a;
-				node = document.createElement("div");
-				text = document.createTextNode(grade);
-				node.appendChild(text);
-				node.className = 'grade-overlay'
-				//node.style = "position:absolute; top:0; left:0; text-align:center; font-size:15pt; color:white; padding:10px; background-color:black; border-radius: 0 0 25px 0; width: 75px;";
-				b.appendChild(node);
+	try {
+		if(window.location.href.split('/').length > 4)
+			return;
+		function httpGet()
+		{
+			var xmlHttp = new XMLHttpRequest();
+			xmlHttp.open( "GET", mainCanvasURL+'/api/v1/users/self/courses?include[]=total_scores&enrollment_state=active&per_page=20', false ); // false for synchronous request
+			xmlHttp.send( null );
+			getParts(xmlHttp.response);
+		}
+		function getParts(response){
+			response = JSON.parse(response);
+			response.forEach(function t(x){
+				temp = getGradeText(x)
+				setGrade(temp[0], temp[1])
+			});
+		}
+		function setGrade(name, grade){
+			elementArr = document.getElementsByClassName("ic-DashboardCard");
+			for(let i = 0; i < elementArr.length; i++){
+				x = elementArr[i];
+				if(name == x.getAttribute("aria-label")){
+					a = x.children[0];
+					b = a;
+					node = document.createElement("div");
+					text = document.createTextNode(grade);
+					node.appendChild(text);
+					node.className = 'grade-overlay'
+					//node.style = "position:absolute; top:0; left:0; text-align:center; font-size:15pt; color:white; padding:10px; background-color:black; border-radius: 0 0 25px 0; width: 75px;";
+					b.appendChild(node);
+				}
 			}
 		}
+
+		overlay = document.createElement("div");
+		overlay.className = "CanvasPlus-element";
+		overlay.onload = httpGet();
+
+		document.body.appendChild(overlay);
 	}
+	catch(e){
+		//console.log(e)
+	}
+}
 
-	overlay = document.createElement("div");
-	overlay.className = "CanvasGrades-element";
-	overlay.onload = httpGet();
-
-	return overlay
+function CanvasClassGrade() {
+	try {
+		if(!window.location.href.includes('courses'))
+			return;
+		
+		function httpGet(id)
+		{
+			var xmlHttp = new XMLHttpRequest();
+			xmlHttp.open( "GET", mainCanvasURL+'/api/v1/courses/'+id+'?include[]=total_scores', false ); // false for synchronous request
+			xmlHttp.send( null );
+			return xmlHttp.response;
+		}
+		courseID = window.location.href.split('/')
+		courseID = courseID[courseID.indexOf('courses')+1]
+		console.log(getGradeText(JSON.parse(httpGet(courseID))))
+		div = document.createElement('div')
+		text = document.createTextNode(getGradeText(JSON.parse(httpGet(courseID))))
+		div.appendChild(text)
+		div.className = 'current-grade';
+		document.getElementsByClassName('ic-app-nav-toggle-and-crumbs')[0].appendChild(div);
+	}
+	catch(e){
+		console.log(e)
+	}
 }
 
 const init = function(){
+	CanvasGradeOverlays();
+	CanvasClassGrade();
 	// inject = document.createElement("div");
 	// inject.className = "CanvasGrades-element";
 	// inject.onload = httpGet();
-	document.body.appendChild(CanvasGradeOverlays());
+	
 	console.log("Injection Complete");
 }
-init();
+try {
+	init();
+}
+catch(e){
+	console.log(e);
+}
